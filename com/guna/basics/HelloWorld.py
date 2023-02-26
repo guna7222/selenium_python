7bae838511bfa4f643da32b79aecc9825af25f52
unknown <rrgunasekhar@gmail.com>
2023-02-26 19:05:21 +0530

    print statement and format method examples

---
+++ com/guna/basics/HelloWorld.py
@@ -0,0 +1,12 @@
+print("Hello World")
+age = 23
+name = "gunasekhar"
+print(age, name)
+print("My name is {} and I'm {} years old".format(name, age))
+name_age = "My name is {} and I'm {} years old".format(name, age)
+print(name_age)
+
+# if you want to know the data type of the variable then we can use type function
+age = 23
+data_type_of_age = type(age)
+print(data_type_of_age)
